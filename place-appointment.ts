0e32d4b7d94e6a3e5f4a336158adccc04632885f
MummireddygariLalathi <60088219+MummireddygariLalathi@users.noreply.github.com>
2020-02-29 02:59:27 -0800

    Add files via upload

---
+++ place-appointment.ts
@@ -0,0 +1,9 @@
+export class PlaceAppointment {
+        public name:number;
+        public address:string;
+        public city:string;
+        public package:string;
+        public TrainerPreference:string;
+        public phone:number;
+    }
+
